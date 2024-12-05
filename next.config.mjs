2d915977fb0a5570249f4e3bd45fe90edcd07af2
nicollely <danielmartirez97@gmail.com>
2024-12-05 08:04:28 +0800

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["digital-affinity.s3.us-east-1.amazonaws.com", "lh3.googleusercontent.com"]
+    }
+};
+
+export default nextConfig;
